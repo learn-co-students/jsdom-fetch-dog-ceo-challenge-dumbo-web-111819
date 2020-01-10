4053fd7d99ce1a3d1746617f4da9d3242241c29e
MohammedQasim <mqshaikh8@gmail.com>
2020-01-10 08:58:53 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-
-document.addEventListener("DOMContentLoaded",(event) => {
-    
-    imgCreator()
-    breedLister()
-    let searchLetter = document.querySelector("#breed-dropdown")
-    console.log(searchLetter)
-    // searchLetter.addEventListener("click",function())
+const imgUrl = "https://dog.ceo/api/breeds/list/all"
+let thePromisedObj = fetch(imgUrl)
+document.addEventListener("DOMContentLoaded", (event) => {
+    thePromisedObj.then(r => r.json()) 
+    .then(turnJSonToHTML)
     let selection = document.querySelector('#breed-dropdown')
     selection.addEventListener('change', (event) => {
         console.log(selection.value)
@@ -19,61 +16,11 @@ document.addEventListener("DOMContentLoaded",(event) => {
             }
         })
     }  )
-  
-
-})
-
-
-
-
-function imgCreator(){
-    let imgDiv = document.querySelector("#dog-image-container")
-    fetch("https://dog.ceo/api/breeds/image/random/4")
-    .then(resp => resp.json())
-    .then(obj => {
-        obj.message.forEach(function(element){
-        newImgTag = document.createElement("img")
-        newImgTag.src = element
-        // newImgTag.alt= "Test"
-        
-      
-        imgDiv.append(newImgTag)
-       
-        })
-       
-    })
-}
-
-
-
-function breedLister (){
-    let ulTag = document.querySelector("#dog-breeds")
-    fetch('https://dog.ceo/api/breeds/list/all')
-    .then(resp => resp.json())
-    .then(obj => {
-        Object.keys(obj.message).forEach(function(element){
-        newLi = document.createElement("li")
-        newLi.innerText = element
-        ulTag.append(newLi)
-        })
-      
- 
-       
-    })
-    
-    ulTag.addEventListener("click",function(e){ 
-        if(e.target.tagName === "LI"){
-            if(e.target.style.color === "green"){  
-                    e.target.style.color = "red"
-            }
-            else{
-                e.target.style.color = "green"
-                }
-            }
-    })
-
-
-}
-
-// obj.messages
-
+  })
+let turnJSonToHTML = (jsonObj) => { 
+    Object.keys(jsonObj.message).forEach(function(element) {
+    let dogUl = document.querySelector("#dog-breeds")
+    let newLi = document.createElement('li')
+    newLi.innerText = element
+    dogUl.append(newLi)
+}      ) }
